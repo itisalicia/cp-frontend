4028fcfe55f8e31a9efe607e9f2b43241d6e18df
ALICIA <alicia.magro@gmail.com>
2025-10-28 10:13:59 +0100

    feat: start header section

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export function Header() {
   return (
     <header className="header">
-      <h1>Checkpoint : frontend</h1>
+      <h1>Countries app</h1>
       <Link to="/">Countries</Link>
     </header>
   );
